697e85db1b39f879c8c9be8b0aa2f85da29f2b35
FCAX <35535240+FCAX@users.noreply.github.com>
2018-02-25 23:21:48 +0800

    Unity Game

---
+++ SmallGame/Assets/Script/Camera/CameraMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    private GameObject lastobj;
+
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, player.transform.position, out hit))
+        {
+            GameObject obj = hit.collider.gameObject;
+            Debug.Log(obj.tag);
+            if (obj.tag == "Building")
+            {
+                Color color = obj.GetComponent<Renderer>().material.color;
+                color.a = 0.5f;
+                obj.GetComponent<Renderer>().material.SetColor("_Color", color);
+
+                lastobj = obj;
+            }
+            else
+            {
+                if (lastobj != null)
+                {
+                    Color color = lastobj.GetComponent<Renderer>().material.color;
+                    color.a = 1f;
+                    lastobj.GetComponent<Renderer>().material.SetColor("_Color", color);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ SmallGame/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Move(hor, ver);
+        Animating(hor, ver);
+        Turning(hor, ver);
+        
+    }
+
+    void Move(float hor, float ver)
+    {
+        movement.Set(hor, 0f, ver);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Animating(float hor, float ver)
+    {
+        bool walking = hor != 0f || ver != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+
+    void Turning(float hor, float ver)
+    {
+        if (hor == 0f & ver == 0f)
+        {
+            return;
+        }
+        movement.Set(hor, 0f, ver);
+        Quaternion playerRotatation = Quaternion.LookRotation(movement);
+        playerRigidbody.MoveRotation(playerRotatation);
+    }
+}
